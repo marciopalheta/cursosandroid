821834427c6b613c523bc4b86d829a96eb25a91d
marciopalheta <marcio.palheta@gmail.com>
2013-12-13 10:38:11 -0400

    Inclusao do modulo WEB

--- CadastroAluno/src/br/com/cursoandroid/cadastroaluno/converter/AlunoConverter.java
+++ CadastroAluno/src/br/com/cursoandroid/cadastroaluno/converter/AlunoConverter.java
@@ -1,5 +1,4 @@
 package br.com.cursoandroid.cadastroaluno.converter;
-
 import java.util.List;
 
 import org.json.JSONException;
@@ -7,11 +6,7 @@ import org.json.JSONStringer;
 
 import android.util.Log;
 import br.com.cursoandroid.cadastroaluno.modelo.bean.Aluno;
-
 public class AlunoConverter {
-
-	private final String TAG = "CADASTRO_ALUNO";
-
 	public String toJSON(List<Aluno> listaAlunos) {
 		try {
 			JSONStringer jsonStringer = new JSONStringer();
@@ -21,20 +16,20 @@ public class AlunoConverter {
 			for (Aluno aluno : listaAlunos) {
 				//Carregar dados do aluno no JSON
 				jsonStringer.object()
-						.key("id").value(aluno.getId())
-						.key("nome").value(aluno.getNome())
-						.key("telefone").value(aluno.getTelefone())
-						.key("endereco").value(aluno.getEndereco())
-						.key("site").value(aluno.getSite())
-						.key("email").value(aluno.getEmail())
-						.key("nota").value(aluno.getNota())
+					.key("id").value(aluno.getId())
+					.key("nome").value(aluno.getNome())
+					.key("telefone").value(aluno.getTelefone())
+					.key("endereco").value(aluno.getEndereco())
+					.key("site").value(aluno.getSite())
+					.key("email").value(aluno.getEmail())
+					.key("nota").value(aluno.getNota())
 				.endObject();
 			}
-			jsonStringer.endArray().endObject().endArray().endObject();
-			System.out.println(jsonStringer.toString());
+			jsonStringer.endArray().endObject()
+				.endArray().endObject();
 			return jsonStringer.toString();
 		} catch (JSONException e) {
-			Log.i(TAG, e.getMessage());
+			Log.i("CADASTRO_ALUNO", e.getMessage());
 			return null;
 		}
 	}
--- CadastroAluno/src/br/com/cursoandroid/cadastroaluno/suport/WebClient.java
+++ CadastroAluno/src/br/com/cursoandroid/cadastroaluno/suport/WebClient.java
@@ -1,38 +1,33 @@
 package br.com.cursoandroid.cadastroaluno.suport;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-
 public class WebClient {
-
 	private final String url;
-
 	public WebClient(String url) {
 		this.url = url;
 	}
-	
 	public String post(String json){
+		//Definicoes de comunicacao
 		DefaultHttpClient httpClient = new DefaultHttpClient();
-		
 		HttpPost post = new HttpPost(url);
 		try {
+			//Coloca a String JSON no conteudo a ser enviado
 			post.setEntity(new StringEntity(json));
 			//Informa que o conteudo da requisicao eh JSON e 
-			//Solicita que a resposta tambem seja em JSON
 			post.setHeader("Content-type", "application/json");
+			//Solicita que a resposta tambem seja em JSON
 			post.setHeader("Accept", "application/json");
-			//Execucao da requisicao
+			//Envio do JSON para o server
 			HttpResponse response = httpClient.execute(post);
 			//Verificacao da reposta
-			String jsonResposta = EntityUtils.toString(response.getEntity());
-			
+			String jsonResposta = 
+					EntityUtils.toString(response.getEntity());
 			return jsonResposta;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		
 	}
 }
